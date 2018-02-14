9da2672cb7f22d816ebe7df608920b5b621e4bb6
Franklin Crosby <obleo33@gmail.com>
2018-02-14 13:25:02 -0700

    Move saveLocation action to action file

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,19 +1,16 @@
 import { connect } from 'react-redux'
 import Form from '../components/Form'
+import { bindActionCreators } from 'redux';
+import { saveLocation } from '../actions/locationActions';
+import { addLocation } from '../actions/locationActions';
+
 
 const mapStateToProps = (state) => {
   return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    saveLocation: (location) => {
-      dispatch({
-        type: 'SAVE_LOCATION',
-        data: location,
-      });
-    },
-  };
+  return bindActionCreators({ saveLocation, addLocation }, dispatch)
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form);
